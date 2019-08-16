99b0f54b98ff7ed882fe18cd09b69d46cd4d5940
Jaemin Choi <choi18@llnl.gov>
2019-08-16 11:11:43 -0700

    Update roofline script

--- scripts/loh1-roofline.sh
+++ scripts/loh1-roofline.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#BSUB -nnodes 1
+#BSUB -nnodes 4
 #BSUB -core_isolation 2
-#BSUB -W 120
+#BSUB -W 240
 #BSUB -G asccasc
-#BSUB -J loh1-rf
+#BSUB -J rf-n4-t4
 #BSUB -q pbatch
 
 date; hostname
 echo -n 'Job ID is '; echo $LSB_JOBID
 cd /g/g90/choi18/gpuroofperf-toolkit/tool
 
-python3 gpuroofperf-cli.py -o loh1-h100.json /g/g90/choi18/sw4lite/optimize_cuda_lassen/sw4lite /g/g90/choi18/sw4lite/tests/loh1/LOH.1-h100.in
+python3 gpuroofperf-cli.py -x "lrun -N4 -T4" -o loh1-h63-l1bypass-N4T4.json /g/g90/choi18/sw4lite/optimize_cuda_lassen_l1bypass/sw4lite /g/g90/choi18/sw4lite/tests/loh1/LOH.1-h39.in
 
 echo 'Done'
